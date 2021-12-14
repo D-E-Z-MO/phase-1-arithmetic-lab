cb2be2fe3c3c7318604312d4e8913e5d9164f851
douglassmoss <douglassmoss@icloud.com>
2021-12-13 18:22:39 -0800

    commit tree 32bcccb6613a85f88edb0ad77274674a9a2498c3

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2
+const random = math.floor(Math.random());
+const num3 = 5;
+const num4 = 23;
+const mod = 4
+;
+const array1 = [1, 2, 3, 20]
+const max = Math.max(...array1)
